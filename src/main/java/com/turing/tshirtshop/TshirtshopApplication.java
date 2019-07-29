6ccec176a22ffe9953369d55ce32f1a2bfee8ca6
System Administrator <root@Maxs-MacBook-Pro.local>
2019-07-29 11:34:42 -0400

    Initializing project

---
+++ src/main/java/com/turing/tshirtshop/TshirtshopApplication.java
@@ -0,0 +1,13 @@
+package com.turing.tshirtshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TshirtshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TshirtshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/turing/tshirtshop/TshirtshopApplicationTests.java
@@ -0,0 +1,16 @@
+package com.turing.tshirtshop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TshirtshopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
